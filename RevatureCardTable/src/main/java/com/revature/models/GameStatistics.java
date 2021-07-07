207752a5cd14d9323691b018ee6999bac16a65d2
molliemorrow <mfmorrow97@gmail.com>
2021-07-07 14:11:48 -0700

    Variables for models from ER diagram

--- RevatureCardTable/src/main/java/com/revature/models/GameStatistics.java
+++ RevatureCardTable/src/main/java/com/revature/models/GameStatistics.java
@@ -1,5 +1,17 @@
 package com.revature.models;
 
+import java.sql.Date;
+
 public class GameStatistics {
+	
+	private int id;
+	
+	private int userID;
+	
+	private int points;
+	
+	private boolean won;
+	
+	private Date datePlayed;
 
 }
--- RevatureCardTable/src/main/java/com/revature/models/User.java
+++ RevatureCardTable/src/main/java/com/revature/models/User.java
@@ -1,5 +1,19 @@
 package com.revature.models;
 
+import java.util.List;
+
 public class User {
+	
+	private int id;
+	
+	private String username;
+	
+	private String password;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private List<GameStatistics> gameStats;
 
 }
