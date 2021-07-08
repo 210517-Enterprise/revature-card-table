9666f22a7deb71569a0b0c18f35cbce139014cf0
vincedgm <vince.012999@gmail.com>
2021-07-08 10:30:25 -0400

    Basic Additional Queries

--- RevatureCardTable/src/main/java/com/revature/repository/GameStatisticsDAO.java
+++ RevatureCardTable/src/main/java/com/revature/repository/GameStatisticsDAO.java
@@ -1,9 +1,18 @@
 package com.revature.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.revature.models.GameStatistics;
 
 public interface GameStatisticsDAO extends JpaRepository<GameStatistics, Integer>{
 
+	@Query("SELECT SUM(g.won) FROM GameStatistics g where g.userID = :userID AND g.gameName = :gameName")
+	int findNumberOfWinsByUserIDAndGameName(@Param("userID") int userID, @Param("gameName") String gameName);
+	
+	@Query("SELECT * FROM GameStatistics g WHERE g.gameName = :gameName")
+	List<GameStatistics> findByGameName(@Param("gameName") String gameName);
 }
