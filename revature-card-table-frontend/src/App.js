d2c5762361c2f34eba8319b4f780671a7446a9e9
ajang06 <aronjang97@gmail.com>
2021-07-09 22:49:47 -0500

    Frontend Displays a Table of All Games

--- revature-card-table-frontend/src/App.js
+++ revature-card-table-frontend/src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// export default class gameChoice extends React.Component {
+//   constructor() {
+//     super();
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     const data = new FormData(event.target);
+
+//     fetch('/api/form-submit-url', {
+//       method: 'POST',
+//       body: data,
+//     });
+//   }
+
+//   render() {
+
+//     return (
+//       <div>
+//         <h1>Which Game Records Do You Want?</h1>
+
+//         <form action={this.handleSubmit}>
+//           <label for="game">Choose a Game: </label>
+//           <select name="game" id="game">
+//           <option value="gofish">Go Fish</option>
+//           <option value="war">War</option>
+//           <option value="speed">Speed</option>
+//           <option value="52 card pickup">52 Card Pick Up</option>
+//           </select>
+//           <br/><br/>
+//           <input type="submit" value="Submit"></input>
+//         </form>
+//        </div>
+//     );
+//   }
+// }
+
+export default class GameStatistics extends React.Component {
+  state = {
+    gameStats: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:8080/revature-card-table/leaderboard/find-all`)
+      .then((res) => {
+        const gameStats = res.data;
+        this.setState({ gameStats });
+        console.log(this.state.gameStats);
+      });
+  }
+
+  render() {
+    return (
+      <div >
+        <h1 class="text-center">Table of All Games</h1>
+        <table class ="table">
+          <thead>
+            <tr>
+              <th>User ID</th>
+              <th>Game</th>
+              <th>Points</th>
+              <th>Won?</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.gameStats.map((gameStat) => {
+              return (
+                <tr>
+                  <td>{gameStat.id}</td>
+                  <td>{gameStat.gameName}</td>
+                  <td>{gameStat.points}</td>
+                  <td>
+                    <input type="checkbox" checked={gameStat.won} />
+                  </td>
+                  <td>{gameStat.datePlayed}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
-export default App;
