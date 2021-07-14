bd009f18487577d2f4332b9d330a22995f9dcf0e
ajang06 <aronjang97@gmail.com>
2021-07-13 22:06:02 -0500

    NavBar that actually works

--- revature-card-table-frontend/src/Component/BootstrapNavbar.js
+++ revature-card-table-frontend/src/Component/BootstrapNavbar.js
@@ -4,14 +4,14 @@ import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 class BootstrapNavbar extends React.Component {
   render() {
     return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar collapseOnSelect expand="xl" bg="dark" variant="dark" fixed="top">
+        <Container fluid>
+          <Navbar.Brand href="#home">Revature Card Table</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <Nav.Link href="#login">Login</Nav.Link>
+              <Nav.Link href="#register">Register</Nav.Link>
               <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">
