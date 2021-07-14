6b3127a3e3f6b2d06621035194937c713610a838
vincedgm <vince.012999@gmail.com>
2021-07-14 17:06:40 -0400

    Initial Register, no validation

--- revature-card-table-frontend/src/Component/Register.js
+++ revature-card-table-frontend/src/Component/Register.js
@@ -1,3 +1,40 @@
+import { useState, useEffect } from "react";
+import {useForm} from 'react-hook-form';
+
 export default function Register() {
-    return <h1>Register Page</h1>
+    const {register, handleSubmit} = useForm();
+
+    const onSubmit = ((newUser) => {
+        fetch("http://localhost:8080/revature-card-table/users/register", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        }).then((response) => {
+            console.log(response);
+        }).catch((err) => console.log(err));
+    });
+
+    const checkKeyDown = (e) => {
+        if (e.code === 'Enter') e.preventDefault();
+      };
+
+    return (
+        <>
+            <h1>Register Page</h1>
+            <form onSubmit={handleSubmit(onSubmit)} onKeyDown={(e) => checkKeyDown(e)}>
+                <label>Username: <input {...register("username")}/></label> 
+                <br/>
+                <label>Password: <input type="password" {...register("password")}/></label>
+                <br/>
+                <label>First Name: <input {...register("firstName")}/></label>
+                <br/>
+                <label>Last Name: <input {...register("lastName")}/></label>
+                <br/>
+                <input type="submit"/>
+            </form>
+        </>
+    );
 }
