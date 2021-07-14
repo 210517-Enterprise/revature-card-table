c130bbb2da3ddea42413cefdfb43ad3776bfed0d
vincedgm <vince.012999@gmail.com>
2021-07-14 15:53:12 -0400

    NavBar with Routing

--- revature-card-table-frontend/src/App.js
+++ revature-card-table-frontend/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from "react";
-
 import { Layout, Header } from "./Layout.components";
 import BootstrapNavbar from "./Component/BootstrapNavbar";
+import {Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Home from './Component/Home';
+import Login from './Component/Login';
+import Register from './Component/Register';
+import {useState} from 'react';
 
-class App extends Component {
-  render() {
+
+export default function App() {
+  const [loggedIn, updateLogin] = useState(false);
+
     return (
       <div className="App">
-        <Layout>
-          <BootstrapNavbar />
-        </Layout>
+        <BootstrapNavbar isLoggedIn={false}/>
+        <Switch>
+          <Route path='/' exact><Home isLoggedIn={false}/></Route>
+          <Route path='/login' exact component={Login}></Route>
+          <Route path='/register' exact component={Register}></Route>
+          
+          {/* {loggedIn && (<Route path='/war' exact component={War}></Route>)} */}
+        </Switch>
       </div>
     );
-  }
 }
 
-export default App;
