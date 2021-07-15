31c10fc61eccfe950a9d73826febc24adc481718
molliemorrow <mfmorrow97@gmail.com>
2021-07-15 13:46:04 -0700

    Managed login token and some minor error handling for LoginDisplay

--- revature-card-table-frontend/src/Component/LoginDisplay.js
+++ revature-card-table-frontend/src/Component/LoginDisplay.js
@@ -3,39 +3,44 @@ import { useForm } from "react-hook-form";
 import { Form, Button } from "react-bootstrap";
 import axios from "axios";
 import { useHistory} from "react-router-dom";
+import { render } from "@testing-library/react";
 
 export default function LoginDisplay({setToken}) {
     const { register, handleSubmit } = useForm();
-    
-    const [username, updateUsername] = useState("");
-    const [password, updatePassword] = useState("");
-    const [responseUser, updateResponseUser] = useState(null);
     const history = useHistory();
 
-    const onSubmit = (user) => {
-        updateUsername(user.username);
-        updatePassword(user.password);
-
-        console.log(username);
-        console.log(password);
+    const onSubmit = (user) => {    
 
         axios
-        .get(`http://localhost:8080/revature-card-table/users/login?username=${username}&password=${password}`)
+        .get(`http://localhost:8080/revature-card-table/users/login?username=${user.username}&password=${user.password}`)
         .then(response => {
-            console.log(response);
-            updateResponseUser(response.data);
-            setToken({
-                username: `${user.username}`,
-                id: `${response.data.user_id}`,
-                isLoggedIn: true
-            });
-            history.push("/");
+            if(response.data.user_id === undefined){
+                console.log("Failure");
+                let error_p = document.getElementById("error");
+                error_p.innerText = "Username or password are incorrect";
+
+                let usr_input = document.getElementById("username");
+                usr_input.value = "";
+                usr_input.style.backgroundColor = "#ff968f";
+
+                let pwd_input = document.getElementById("password");
+                pwd_input.value = "";
+                pwd_input.style.backgroundColor = "#ff968f";
+
+            } 
+            else{
+                setToken({
+                    username: `${user.username}`,
+                    id: `${response.data.user_id}`,
+                    isLoggedIn: true
+                });
+                history.push("/");
+            }
         })
         .catch(error => {
-            console.log(error);
+            console.log("Failure to login user",error);
         });
 
-        console.log(responseUser);
     }
 
     return (
@@ -43,14 +48,15 @@ export default function LoginDisplay({setToken}) {
             <Form>
                 <Form.Group>
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Enter your username" {...register("username", {required: true})}/>
+                    <Form.Control id="username" type="text" placeholder="Enter your username" {...register("username", {required: true})}/>
                 </Form.Group>
 
                 <Form.Group>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter your password" {...register("password", {required: true})}/>
+                    <Form.Control id="password" type="password" placeholder="Enter your password" {...register("password", {required: true})}/>
                 </Form.Group>
 
+                <p id="error"></p>
                 <Button variant="primary" type="submit" onClick={handleSubmit(onSubmit)}>
                     Login
                 </Button>
