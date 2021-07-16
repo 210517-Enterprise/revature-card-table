4b7495b6208b3f0fb53303a6b398d7204ecf0029
vincedgm <vince.012999@gmail.com>
2021-07-16 18:05:27 -0400

    Initial Speed Commit

--- revature-card-table-frontend/src/Component/AppRouter.js
+++ revature-card-table-frontend/src/Component/AppRouter.js
@@ -2,6 +2,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import LoginDisplay from "./LoginDisplay";
 import Register from "./Register";
+import Speed from "./Speed";
 
 export default function AppRouter({ isLoggedIn, setToken }) {
   return (
@@ -15,6 +16,7 @@ export default function AppRouter({ isLoggedIn, setToken }) {
         </Route>
         <Route path="/register" exact component={Register}></Route>
         <Route path="/war" exact component={War}></Route>
+        <Route path="/speed/" exact component={Speed}></Route>
 
         {/* {loggedIn && (<Route path='/war' exact component={War}></Route>)} */}
       </Switch>
