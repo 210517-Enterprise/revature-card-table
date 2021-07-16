f98e1b18db8ff6e36e8fff4255b5b6758f50e41d
vincedgm <vince.012999@gmail.com>
2021-07-15 21:02:59 -0400

    Logout works

--- revature-card-table-frontend/src/Component/BootstrapNavbar.js
+++ revature-card-table-frontend/src/Component/BootstrapNavbar.js
@@ -3,6 +3,15 @@ import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 export default function BootstrapNavbar({ isLoggedIn, setToken }) {
+
+  function logOut() {
+    setToken({
+      username: ``,
+      id: ``,
+      isLoggedIn: false,
+    });
+  }
+
   return (
     <Navbar
       style={{ position: "sticky" }}
@@ -53,7 +62,7 @@ export default function BootstrapNavbar({ isLoggedIn, setToken }) {
             )}
             {isLoggedIn && (
               <LinkContainer to="/">
-                <Nav.Link>Logout</Nav.Link>
+                <Nav.Link onSelect={logOut}>Logout</Nav.Link>
               </LinkContainer>
             )}
           </Nav>
@@ -63,10 +72,3 @@ export default function BootstrapNavbar({ isLoggedIn, setToken }) {
   );
 }
 
-function logOut({setToken}) {
-  setToken({
-    username: ``,
-    id: ``,
-    isLoggedIn: false,
-  });
-}
