ac9f7bed8c84137175077715e660b971d8e45c56
ajang06 <aronjang97@gmail.com>
2021-07-15 19:49:21 -0500

    my-account end point Added

--- revature-card-table-frontend/src/Component/BootstrapNavbar.js
+++ revature-card-table-frontend/src/Component/BootstrapNavbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-export default function BootstrapNavbar({ isLoggedIn }) {
+export default function BootstrapNavbar({ isLoggedIn, setToken }) {
   return (
     <Navbar
       style={{ position: "sticky" }}
@@ -13,7 +13,9 @@ export default function BootstrapNavbar({ isLoggedIn }) {
     >
       <Container fluid>
         <LinkContainer to="/">
-          <Navbar.Brand><img src = "/Picture1.png" width="150"/></Navbar.Brand>
+          <Navbar.Brand>
+            <img src="/Picture1.png" width="150" />
+          </Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -38,20 +40,32 @@ export default function BootstrapNavbar({ isLoggedIn }) {
                   52 Card Pickup
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Etc.</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.4">Rules</NavDropdown.Item>
               </NavDropdown>
             )}
           </Nav>
           <Nav className="ml-auto">
-            {isLoggedIn && <Nav.Link href="#deets">My Account</Nav.Link>}
             {isLoggedIn && (
-              <Nav.Link eventKey={2} href="#memes">
-                Logout
-              </Nav.Link>
+              <LinkContainer to="/my-account">
+                <Nav.Link>My Account</Nav.Link>
+              </LinkContainer>
+            )}
+            {isLoggedIn && (
+              <LinkContainer to="/">
+                <Nav.Link>Logout</Nav.Link>
+              </LinkContainer>
             )}
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
+}
+
+function logOut({setToken}) {
+  setToken({
+    username: ``,
+    id: ``,
+    isLoggedIn: false,
+  });
 }
