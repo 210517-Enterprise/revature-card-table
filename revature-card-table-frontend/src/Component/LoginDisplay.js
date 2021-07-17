72d88f6d513e6eff423f3f07dc4e46dc01f6bf3a
molliemorrow <mfmorrow97@gmail.com>
2021-07-16 18:52:54 -0700

    Login component error fucntionality

--- revature-card-table-frontend/src/Component/LoginDisplay.js
+++ revature-card-table-frontend/src/Component/LoginDisplay.js
@@ -1,106 +1,118 @@
 import { useForm } from "react-hook-form";
-import { Form, Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
-import { render } from "@testing-library/react";
+//import { render } from "@testing-library/react";
 import "../CSS/LoginDisplay.css";
 import { motion } from "framer-motion";
-import styled from "styled-components";
-import Spades from "../Images/spades1.png";
-import Hearts from "../Images/hearts1.png";
-import Clubs from "../Images/clubs1.png";
-import Diamonds from "../Images/diamonds1.png";
-export default function LoginDisplay({ setToken }) {
+// import styled from "styled-components";
+// import Spades from "../Images/spades1.png";
+// import Hearts from "../Images/hearts1.png";
+// import Clubs from "../Images/clubs1.png";
+// import Diamonds from "../Images/diamonds1.png";
+export default function LoginDisplay({ setToken }) {    
   const { register, handleSubmit } = useForm();
-  const { registerUN, handleForgotPwd} = useForm();
   const history = useHistory();
+
   const onSubmit = (user) => {
-    axios
-      .get(
-        `http://localhost:8080/revature-card-table/users/login?username=${user.username}&password=${user.password}`
-      )
-      .then((response) => {
-        if (response.data.user_id === undefined) {
-          console.log("Failure");
-          let error_p = document.getElementById("error");
-          error_p.innerText =
-            "Authentication Error - Check your Username and Password";
-          error_p.style.color = "#ff968f";
-          let usr_input = document.getElementById("username");
-          usr_input.value = "";
-          usr_input.style.backgroundColor = "#ff968f";
-          let pwd_input = document.getElementById("password");
-          pwd_input.value = "";
-          pwd_input.style.backgroundColor = "#ff968f";
-        } else {
-          setToken({
-            username: `${user.username}`,
-            id: `${response.data.user_id}`,
-            first_name: `${response.data.firstName}`,
-            last_name: `${response.data.lastName}`,
-            isLoggedIn: true,
-          });
-          history.push("/");
-        }
-      })
-      .catch((error) => {
-        console.log("Failure to login user", error);
-      });
-  };
+    if(user.username && user.password){
+      axios.get(`http://localhost:8080/revature-card-table/users/login?username=${user.username}&password=${user.password}`)
+        .then((response) => {
+          if (response.data.user_id === undefined) {
+            let error_p = document.getElementById("error");
+            error_p.innerText = "Authentication Error - Check your Username and Password";
+            error_p.style.color = "#ff968f";
+
+            let usr_input = document.getElementById("username");
+            usr_input.value = "";
+            usr_input.style.backgroundColor = "#ff968f";
+
+            let pwd_input = document.getElementById("password");
+            pwd_input.value = "";
+            pwd_input.style.backgroundColor = "#ff968f";
+          } else {
+            setToken({
+              username: `${user.username}`,
+              id: `${response.data.user_id}`,
+              first_name: `${response.data.firstName}`,
+              last_name: `${response.data.lastName}`,
+              isLoggedIn: true,
+            });
+            history.push("/");
+          }
+        })
+        .catch((error) => {
+          console.log("Failure to login user", error);
+        });
+    }
+
+    if(!user.username){
+      let error_p = document.getElementById("error");
+      error_p.innerText = "Authentication Error -  Complete the form to login";
+      error_p.style.color = "#ff968f";
+
+      let usr_input = document.getElementById("username");
+      usr_input.value = "";
+      usr_input.style.backgroundColor = "#ff968f";
+    }
+
+    if(!user.username){
+      let error_p = document.getElementById("error");
+      error_p.innerText = "Authentication Error - Complete the form to login";
+      error_p.style.color = "#ff968f";
+
+      let usr_input = document.getElementById("password");
+      usr_input.value = "";
+      usr_input.style.backgroundColor = "#ff968f";
+    }
+  }
+
   const fadeLeft = {
     hidden: { opacity: 0, x: -100 },
     visible: { opacity: 1, x: 0 },
-  };
+  }
+
   const updateToken = (user) => {
-    console.log("updating token w " + user.username);
-    setToken({
-      username: `${user.username}`,
-      id: "",
-      first_name: "",
-      last_name: "",
-      isLoggedIn: false
-    })
-    history.push("/forgot-password");
+    if(user.username === ""){
+      let error_p = document.getElementById("error");
+          error_p.innerText = "Enter your username to reset your password";;
+          error_p.style.color = "#ff968f";
+    }
+    else{
+      setToken({
+        username: `${user.username}`,
+        id: "",
+        first_name: "",
+        last_name: "",
+        isLoggedIn: false
+      });
+      history.push("/forgot-password");
+  }
   }
   return (
     <>
       <div id="page-layout" class="row h-100" padding-top="10em">
-        <motion.div
-          class="col-lg-6 mx-auto"
-          variants={fadeLeft}
-          initial="hidden"
-          animate="visible"
-          transition={{ duration: 1 }}
-        >
-          <br/>
+        <motion.div class="col-lg-6 mx-auto" variants={fadeLeft} initial="hidden" animate="visible"
+          transition={{ duration: 1 }} >
           <Form>
+
             <Form.Group>
               <h2>Log In</h2>
               <br />
               <h5>Username</h5>
-              <Form.Control
-                id="username"
-                type="text"
-                placeholder="Enter username"
-                {...register("username", { required: true })}
-              />
+              <Form.Control id="username" type="text" placeholder="Enter username" onClick={enterUsername}
+                {...register("username", { required: false })} />
             </Form.Group>
+
             <Form.Group>
               <h5>Password</h5>
-              <Form.Control
-                id="password"
-                type="password"
-                placeholder="Enter password"
-                {...register("password", { required: false })}
-              />
+              <Form.Control id="password" type="password" placeholder="Enter password" onClick={enterPassword}
+                {...register("password", { required: false })} />
             </Form.Group>
+
             <p id="error"></p>
-            <motion.button
-              className="custom-btn"
-              type="submit"
-              onClick={handleSubmit(onSubmit)}
-              whileHover={{ scale: 1.05 }}
-            >
+            <motion.button className="btn btn-primary btn-block" type="submit" onClick={handleSubmit(onSubmit)}
+              whileHover={{ scale: 1.05 }} >
               Login
             </motion.button>
             <p className="forgot-password text-right">
@@ -108,6 +120,7 @@ export default function LoginDisplay({ setToken }) {
             </p>
           </Form>
         </motion.div>
+
         {/* <motion.div
           class="col-md-7 my-auto"
           initial={{ opacity: 0 }}
@@ -155,7 +168,18 @@ export default function LoginDisplay({ setToken }) {
             // dragConstraints={{left:-100, right: 100, top: 100, bottom:-100}}
           ></motion.img>
         </motion.div> */}
+
       </div>
     </>
   );
+}
+
+function enterUsername(){
+  let form = document.getElementById("username");
+  form.style.backgroundColor = "white";
+}
+
+function enterPassword(){
+  let form = document.getElementById("password");
+  form.style.backgroundColor = "white";
 }
