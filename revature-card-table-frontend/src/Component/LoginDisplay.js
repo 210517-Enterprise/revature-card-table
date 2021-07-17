26f526ab7b83fb8532229c9a720eec8f3a3e1126
ajang06 <aronjang97@gmail.com>
2021-07-16 21:40:06 -0500

    Login and Registration Animations Added,L&R UI finished?

--- revature-card-table-frontend/src/Component/LoginDisplay.js
+++ revature-card-table-frontend/src/Component/LoginDisplay.js
@@ -10,26 +10,32 @@ import { motion } from "framer-motion";
 // import Hearts from "../Images/hearts1.png";
 // import Clubs from "../Images/clubs1.png";
 // import Diamonds from "../Images/diamonds1.png";
-export default function LoginDisplay({ setToken }) {    
+export default function LoginDisplay({ setToken }) {
   const { register, handleSubmit } = useForm();
   const history = useHistory();
 
   const onSubmit = (user) => {
-    if(user.username && user.password){
-      axios.get(`http://localhost:8080/revature-card-table/users/login?username=${user.username}&password=${user.password}`)
+    if (user.username && user.password) {
+      axios
+        .get(
+          `http://localhost:8080/revature-card-table/users/login?username=${user.username}&password=${user.password}`
+        )
         .then((response) => {
           if (response.data.user_id === undefined) {
             let error_p = document.getElementById("error");
-            error_p.innerText = "Authentication Error - Check your Username and Password";
-            error_p.style.color = "#ff968f";
+            error_p.innerText =
+              "Authentication Error - Check your Username and Password";
+            error_p.style.color = "#ff3322";
 
             let usr_input = document.getElementById("username");
             usr_input.value = "";
-            usr_input.style.backgroundColor = "#ff968f";
+            usr_input.style.borderLeftColor = "#ff3322";
+            usr_input.style.borderLeftWidth = "5px";
 
             let pwd_input = document.getElementById("password");
             pwd_input.value = "";
-            pwd_input.style.backgroundColor = "#ff968f";
+            pwd_input.style.borderLeftColor = "#ff3322";
+            pwd_input.style.borderLeftWidth = "5px";
           } else {
             setToken({
               username: `${user.username}`,
@@ -46,77 +52,101 @@ export default function LoginDisplay({ setToken }) {
         });
     }
 
-    if(!user.username){
+    if (!user.username) {
       let error_p = document.getElementById("error");
-      error_p.innerText = "Authentication Error -  Complete the form to login";
-      error_p.style.color = "#ff968f";
+      error_p.innerText = `Authentication Error
+      Complete the form to login`;
+      error_p.style.color = "#ff3322";
 
       let usr_input = document.getElementById("username");
       usr_input.value = "";
-      usr_input.style.backgroundColor = "#ff968f";
+      usr_input.style.borderLeftColor = "#ff3322";
+      usr_input.style.borderLeftWidth = "5px";
     }
 
-    if(!user.username){
+    if (!user.username) {
       let error_p = document.getElementById("error");
-      error_p.innerText = "Authentication Error - Complete the form to login";
-      error_p.style.color = "#ff968f";
+      error_p.innerText = `Authentication Error
+      Complete the form to login`;
+      error_p.style.color = "#ff3322";
 
-      let usr_input = document.getElementById("password");
-      usr_input.value = "";
-      usr_input.style.backgroundColor = "#ff968f";
+      let pwd_input = document.getElementById("password");
+      pwd_input.value = "";
+      pwd_input.style.borderLeftColor = "#ff3322";
+      pwd_input.style.borderLeftWidth = "5px";
     }
-  }
+  };
 
   const fadeLeft = {
     hidden: { opacity: 0, x: -100 },
     visible: { opacity: 1, x: 0 },
-  }
+  };
 
   const updateToken = (user) => {
-    if(user.username === ""){
+    if (user.username === "") {
       let error_p = document.getElementById("error");
-          error_p.innerText = "Enter your username to reset your password";;
-          error_p.style.color = "#ff968f";
-    }
-    else{
+      error_p.innerText = "Enter your username to reset your password";
+      error_p.style.color = "#ff3322";
+    } else {
       setToken({
         username: `${user.username}`,
         id: "",
         first_name: "",
         last_name: "",
-        isLoggedIn: false
+        isLoggedIn: false,
       });
       history.push("/forgot-password");
-  }
-  }
+    }
+  };
   return (
     <>
       <div id="page-layout" class="row h-100" padding-top="10em">
-        <motion.div class="col-lg-6 mx-auto" variants={fadeLeft} initial="hidden" animate="visible"
-          transition={{ duration: 1 }} >
+        <br />
+        <motion.div
+          class="col-lg-6 mx-auto"
+          variants={fadeLeft}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1 }}
+        >
           <Form>
-
             <Form.Group>
               <h2>Log In</h2>
-              <br />
+
               <h5>Username</h5>
-              <Form.Control id="username" type="text" placeholder="Enter username" onClick={enterUsername}
-                {...register("username", { required: false })} />
+              <Form.Control
+                id="username"
+                type="text"
+                placeholder="Enter username"
+                onClick={enterUsername}
+                {...register("username", { required: false })}
+              />
             </Form.Group>
 
             <Form.Group>
               <h5>Password</h5>
-              <Form.Control id="password" type="password" placeholder="Enter password" onClick={enterPassword}
-                {...register("password", { required: false })} />
+              <Form.Control
+                id="password"
+                type="password"
+                placeholder="Enter password"
+                onClick={enterPassword}
+                {...register("password", { required: false })}
+              />
             </Form.Group>
 
             <p id="error"></p>
-            <motion.button className="btn btn-primary btn-block" type="submit" onClick={handleSubmit(onSubmit)}
-              whileHover={{ scale: 1.05 }} >
+            <motion.button
+              className="custom-btn"
+              type="submit"
+              onClick={handleSubmit(onSubmit)}
+              whileHover={{ scale: 1.05 }}
+            >
               Login
             </motion.button>
             <p className="forgot-password text-right">
-              <a href="/forgot-password" onClick={handleSubmit(updateToken)}>Forgot Password?</a>
+              <a href="/forgot-password" onClick={handleSubmit(updateToken)}>
+                Forgot Password?
+              </a>
             </p>
           </Form>
         </motion.div>
@@ -168,18 +198,17 @@ export default function LoginDisplay({ setToken }) {
             // dragConstraints={{left:-100, right: 100, top: 100, bottom:-100}}
           ></motion.img>
         </motion.div> */}
-
       </div>
     </>
   );
 }
 
-function enterUsername(){
+function enterUsername() {
   let form = document.getElementById("username");
   form.style.backgroundColor = "white";
 }
 
-function enterPassword(){
+function enterPassword() {
   let form = document.getElementById("password");
   form.style.backgroundColor = "white";
 }
--- revature-card-table-frontend/src/Component/Register.js
+++ revature-card-table-frontend/src/Component/Register.js
@@ -3,9 +3,15 @@ import { useForm } from "react-hook-form";
 import { Form, Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
+import "../CSS/LoginDisplay.css";
+import { motion } from "framer-motion";
 
 export default function Register() {
-  const { register, formState: { errors }, handleSubmit, } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
   const headers = {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -13,8 +19,14 @@ export default function Register() {
   const history = useHistory();
 
   const onSubmit = (newUser) => {
-    if(newUser.username && newUser.password && newUser.firstName && newUser.lastName && newUser.securityAnswer){
-        console.log("nothings undefined");
+    if (
+      newUser.username &&
+      newUser.password &&
+      newUser.firstName &&
+      newUser.lastName &&
+      newUser.securityAnswer
+    ) {
+      console.log("nothings undefined");
       axios
         .post(
           "http://localhost:8080/revature-card-table/users/register",
@@ -24,136 +36,206 @@ export default function Register() {
         .then((response) => {
           console.log(response);
           history.push("/");
-          alert("Register Successful! Please login.");
+          alert("Register Successful! You can now login");
         })
         .catch((error) => {
           let error_p = document.getElementById("error");
-          error_p.innerText = `${newUser.username} is Already Taken, Enter a Different Username`;
-          error_p.style.color = "#ff968f";
+          error_p.innerText = `'${newUser.username}' is already taken
+          Please choose a different username`;
+          error_p.style.color = "#ff3322";
         });
     }
-    
-    if(!newUser.username){
+
+    if (!newUser.username) {
       let form = document.getElementById("username");
-      form.style.backgroundColor = "#ff968f";
-      document.getElementById("error").innerText = "Complete the form to register"
+      form.style.borderLeftColor = "#ff3322";
+      form.style.borderLeftWidth = "5px";
+      document.getElementById("error").innerText = `Incomplete Form
+        Please fill out every field`;
+      document.getElementById("error").style.color = "#ff3322";
     }
 
-    if(!newUser.password){
+    if (!newUser.password) {
       let form = document.getElementById("password");
-      form.style.backgroundColor = "#ff968f";
-      document.getElementById("error").innerText = "Complete the form to register"
+      form.style.borderLeftColor = "#ff3322";
+      form.style.borderLeftWidth = "5px";
+      document.getElementById("error").innerText = `Incomplete Form
+      Please fill out every field`;
+      document.getElementById("error").style.color = "#ff3322";
     }
 
-    if(!newUser.firstName){
+    if (!newUser.firstName) {
       let form = document.getElementById("firstName");
-      form.style.backgroundColor = "#ff968f";
-      document.getElementById("error").innerText = "Complete the form to register"
+      form.style.borderLeftColor = "#ff3322";
+      form.style.borderLeftWidth = "5px";
+      document.getElementById("error").innerText = `Incomplete Form
+      Please fill out every field`;
+      document.getElementById("error").style.color = "#ff3322";
     }
 
-    if(!newUser.lastName){
+    if (!newUser.lastName) {
       let form = document.getElementById("lastName");
-      form.style.backgroundColor = "#ff968f";
-      document.getElementById("error").innerText = "Complete the form to register"
+      form.style.borderLeftColor = "#ff3322";
+      form.style.borderLeftWidth = "5px";
+      document.getElementById("error").innerText = `Incomplete Form
+      Please fill out every field`;
+      document.getElementById("error").style.color = "#ff3322";
     }
 
-    if(!newUser.securityAnswer){
+    if (!newUser.securityAnswer) {
       let form = document.getElementById("securityAnswer");
-      form.style.backgroundColor = "#ff968f";
-      document.getElementById("error").innerText = "Complete the form to register"
+      form.style.borderLeftColor = "#ff3322";
+      form.style.borderLeftWidth = "5px";
+      document.getElementById("error").innerText = `Incomplete Form
+      Please fill out every field`;
+      document.getElementById("error").style.color = "#ff3322";
     }
   };
-
 
   const checkKeyDown = (e) => {
     if (e.code === "Enter") e.preventDefault();
   };
-
+  const fadeLeft = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0 },
+  };
   return (
     <>
       <div id="page-layout" class="container-xxl">
         <br />
-        <div class="col-xl-6  mx-auto">
+        <motion.div
+          class="col-xl-6  mx-auto"
+          variants={fadeLeft}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1 }}
+        >
           <Form>
             <Form.Group>
               <h2>Create Account</h2>
               <h5>Username</h5>
-              <Form.Control id="username" type="text" placeholder="Enter username"
-                onClick={enterUsername} {...register("username", { required: false })} />
+              <Form.Control
+                id="username"
+                type="text"
+                placeholder="Enter username"
+                onClick={enterUsername}
+                {...register("username", { required: false })}
+              />
             </Form.Group>
 
             <Form.Group>
               <h5>Password</h5>
-              <Form.Control id="password" type="password" placeholder="Enter password"
-                onClick={enterPassword} {...register("password", { required: false })} />
+              <Form.Control
+                id="password"
+                type="password"
+                placeholder="Enter password"
+                onClick={enterPassword}
+                {...register("password", { required: false })}
+              />
             </Form.Group>
 
             <Form.Group>
               <h5>First Name</h5>
-              <Form.Control id="firstName" type="text" placeholder="Enter first name"
-                onClick={enterFName} {...register("firstName", { required: false })} />
+              <Form.Control
+                id="firstName"
+                type="text"
+                placeholder="Enter first name"
+                onClick={enterFName}
+                {...register("firstName", { required: false })}
+              />
             </Form.Group>
 
             <Form.Group>
               <h5>Last Name</h5>
-              <Form.Control id="lastName" type="test" placeholder="Enter last name" required="true"
-                onClick={enterLName} {...register("lastName", { required: false })} />
+              <Form.Control
+                id="lastName"
+                type="test"
+                placeholder="Enter last name"
+                required="true"
+                onClick={enterLName}
+                {...register("lastName", { required: false })}
+              />
             </Form.Group>
-            
+
             <Form.Group>
               <h5>Security Question</h5>
-              <Form.Control as="select" {...register("securityQuestion", { required: false })}>
-                <option value="What were the last four digits of your childhood telephone number?">What were the last four digits of your childhood telephone number?</option>
-                <option value="In what town or city was your first full time job?">In what town or city was your first full time job?</option>
-                <option value="What is the name of your favorite childhood friend?">What is the name of your favorite childhood friend?</option>
-                <option value="hat was the name of your first stuffed animal?">What was the name of your first stuffed animal?</option>
-              </Form.Control> 
+              <Form.Control
+                as="select"
+                {...register("securityQuestion", { required: false })}
+              >
+                <option value="What were the last four digits of your childhood telephone number?">
+                  What were the last four digits of your childhood telephone
+                  number?
+                </option>
+                <option value="In what town or city was your first full time job?">
+                  In what town or city was your first full time job?
+                </option>
+                <option value="What is the name of your favorite childhood friend?">
+                  What is the name of your favorite childhood friend?
+                </option>
+                <option value="hat was the name of your first stuffed animal?">
+                  What was the name of your first stuffed animal?
+                </option>
+              </Form.Control>
             </Form.Group>
 
             <Form.Group>
-              <h5>Answer for Security Quetsion</h5>
-              <Form.Control id="securityAnswer" type="test" placeholder="Answer" required="true"
-                onClick={enterAnswer} {...register("securityAnswer", { required: false })} />
+              <h5>Security Question Answer</h5>
+              <Form.Control
+                id="securityAnswer"
+                type="test"
+                placeholder="Enter answer"
+                required="true"
+                onClick={enterAnswer}
+                {...register("securityAnswer", { required: false })}
+              />
             </Form.Group>
-
             <p id="error"></p>
-            <Button className="btn btn-primary btn-block" type="submit"
-              onClick={handleSubmit(onSubmit)}>
+            <motion.button
+              className="custom-btn"
+              type="submit"
+              onClick={handleSubmit(onSubmit)}
+              whileHover={{ scale: 1.05 }}
+            >
               Create Account
-            </Button>
+            </motion.button>
 
             <p className="forgot-password text-right">
               Already have an account? <a href="/login">Log In</a>
             </p>
           </Form>
-
-        </div>
+        </motion.div>
       </div>
     </>
   );
 }
 
-function enterUsername(){
+function enterUsername() {
   let form = document.getElementById("username");
-  form.style.backgroundColor = "white";
+  form.style.borderColor = "";
+  form.style.borderWidth = "";
 }
 
-function enterPassword(){
+function enterPassword() {
   let form = document.getElementById("password");
-  form.style.backgroundColor = "white";
+  form.style.borderColor = "";
+  form.style.borderWidth = "";
 }
 
-function enterFName(){
+function enterFName() {
   let form = document.getElementById("firstName");
-  form.style.backgroundColor = "white";
+  form.style.borderColor = "";
+  form.style.borderWidth = "";
 }
 
-function enterLName(){
+function enterLName() {
   let form = document.getElementById("lastName");
-  form.style.backgroundColor = "white";
+  form.style.borderColor = "";
+  form.style.borderWidth = "";
 }
 
-function enterAnswer(){
+function enterAnswer() {
   let form = document.getElementById("securityAnswer");
-  form.style.backgroundColor = "white";
+  form.style.borderColor = "";
+  form.style.borderWidth = "";
 }
