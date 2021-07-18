4db8cb3e4d63fe16bdc1aa05e88369cf143d9006
molliemorrow <mfmorrow97@gmail.com>
2021-07-18 13:30:57 -0700

    made game rules for concentration

---
+++ revature-card-table-frontend/src/Component/ConcentrationRules.js
@@ -0,0 +1,29 @@
+
+
+export default function ConcentrationRules(){
+
+    return (
+        <>
+
+            <div id="page-layout" class="container-fluid">
+                <h1>How to plat Concentration</h1>
+                <p>
+                    <ol>
+                        <li>Click two cards to flip them and see if they are a match</li>
+                        <ul>
+                            <li>If the two cards are a match, the cards will be removed from the table</li>
+                            <li>If the two cards are not a match, the cards will be flipped back</li>
+                        </ul>
+
+                        <li>The game ends when the table is cleared and there are no more cards</li>
+                    </ol>
+
+                    Goal: Try to clear the board with as few guesses as possible
+                </p>
+
+
+            </div>
+
+        </>
+    );
+}
