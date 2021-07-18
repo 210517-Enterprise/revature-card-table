c3ebe57c49d4449034afc726febace7eb1dd8fb4
molliemorrow <mfmorrow97@gmail.com>
2021-07-17 23:25:42 -0700

    Finished MatchingGame gameover alert

--- revature-card-table-frontend/src/Component/SingleMatchingGame.js
+++ revature-card-table-frontend/src/Component/SingleMatchingGame.js
@@ -4,11 +4,14 @@ import axios from "axios";
 import CardBack from "../Images/design2.png"
 import "../CSS/MatchingGame.css"
 import TransparentCard from "../Images/TransparentCard.png"
+import { useHistory } from "react-router-dom";
 
 export default function SingleMatchingGame({ token }){
     const [ userMoves, updateUserMoves ] = useState(0);
     const [ cards, updateCards ] = useState(null);
-    const [ matches, updateMatches ] = useState(12);
+    const [ matches, updateMatches ] = useState(13);
+    const [gameOver, updateGameOver ] = useState(false);
+    const history = useHistory();
 
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
@@ -86,9 +89,56 @@ export default function SingleMatchingGame({ token }){
             updateMatches(matches + 1);
             console.log(matches + " matches found");
 
-            //add some animation to the cards...
-            setTimeout(removeCards, 1000);
-            setTimeout(() => {document.getElementById("userPlayResult").innerText = "~~~~~~~~~~Concentration~~~~~~~~~~"}, 2000);
+            if(matches === 13){
+                console.log("GAME OVER")
+                let timeLapsedInMilliseconds = Date.now() - start;
+                let timeLapsedInSecond = Math.floor(timeLapsedInMilliseconds/1000);
+                let hours = Math.floor(timeLapsedInSecond/3600);
+                let minutes = Math.floor(timeLapsedInSecond /60);
+                let seconds = timeLapsedInSecond % 60;
+    
+                let timer = hours + ":" + minutes + ":" + seconds;
+    
+                console.log(timer);
+                console.log(token);
+                
+    
+                //window.alert(`Game completed in ${userMoves} moves`);
+    
+                axios.get(`http://localhost:8080/revature-card-table/users/${token.username}`)
+                .then((response) => {
+                    console.log(response)
+                    let stats = {
+                        "user": {
+                            "user_id": token.id,
+                            "username": token.username,
+                            "password": response.data.password,
+                            "firstName": token.first_name,
+                            "lastName": token.last_name,
+                            "securityQuestion": response.data.securityQuestion,
+                            "securityAnswer": response.data.securityAnswer
+                        },
+                        "points": userMoves,
+                        "won": true,
+                        "datePlayed": today,
+                        "timeCompleted": timer,
+                        "gameName": "matching-game"
+                    }
+    
+                    console.log(stats);
+    
+                    axios.post("http://localhost:8080/revature-card-table/leaderboard/create",
+                    JSON.stringify(stats), { headers })
+                    .then((response) => {
+                        console.log(response);
+                        updateGameOver(true);
+                      })
+                })
+            }
+            else{setTimeout(removeCards, 1000);
+            setTimeout(() => {document.getElementById("userPlayResult").innerText =
+             "~~~~~~~~~~Concentration~~~~~~~~~~"}, 2000);
+        }
 
         }
         else{
@@ -102,57 +152,7 @@ export default function SingleMatchingGame({ token }){
         console.log(cards);
         cardsFlipped = 0;
 
-        if(matches === 13){
-            console.log("GAME OVER")
-            let timeLapsedInMilliseconds = Date.now() - start;
-            let timeLapsedInSecond = Math.floor(timeLapsedInMilliseconds/1000);
-            let hours = Math.floor(timeLapsedInSecond/3600);
-            let minutes = Math.floor(timeLapsedInSecond /60);
-            let seconds = timeLapsedInSecond % 60;
-
-            let timer = hours + ":" + minutes + ":" + seconds;
-
-            console.log(timer);
-            console.log(token);
-            
-
-            //window.alert(`Game completed in ${userMoves} moves`);
-
-            axios.get(`http://localhost:8080/revature-card-table/users/${token.username}`)
-            .then((response) => {
-                console.log(response)
-                let stats = {
-                    "user": {
-                        "user_id": token.id,
-                        "username": token.username,
-                        "password": response.data.password,
-                        "firstName": token.first_name,
-                        "lastName": token.last_name,
-                        "securityQuestion": response.data.securityQuestion,
-                        "securityAnswer": response.data.securityAnswer
-                    },
-                    "points": userMoves,
-                    "won": true,
-                    "datePlayed": today,
-                    "timeCompleted": timer,
-                    "gameName": "matching-game"
-                }
-
-                console.log(stats);
-
-                axios.post("http://localhost:8080/revature-card-table/leaderboard/create",
-                JSON.stringify(stats), { headers })
-                .then((response) => {
-                    console.log(response);
-                    GameOverAlert();
-                  })
-            })
-
-            
-
-
-
-        }
+        
     }
 
     function resetCards(){
@@ -181,70 +181,134 @@ export default function SingleMatchingGame({ token }){
         let card2 = document.getElementById(usersCards[1].cardID);
         let data2 = document.getElementById(`data${usersCards[1].cardID}`);
         data2.replaceChild(empty, card2);
-    }                                 
+    }   
+
+    function GoHome(){
+        history.push("/");
+    }
+    
 
     return (
         <>
+
             <div id="page-layout" class="container-fluid">
-                <div id="table">
-                    <div class="card-table-div">
 
-                        <h3 id="userPlayResult">~~~~~~~~~~Concentration~~~~~~~~~~</h3>
+                    <div id="table">
+                        
+                        {gameOver ?
+                        (
+                            <>
+                                <Alert id="alert" variant="primary" style={{backgroundColor: "#1d1d35", color: "white"}}>
+                                    You won after {userMoves} guesses!
 
-                        <table cellSpacing="0">
-                            <tr>
-                                <td id="data0"><img id="0" onClick={flip} src={CardBack} /></td>
-                                <td id="data1"><img id="1" onClick={flip} src={CardBack} /></td>
-                                <td id="data2"><img id="2" onClick={flip} src={CardBack} /></td>
-                                <td id="data3"><img id="3" onClick={flip} src={CardBack} /></td>
-                                <td id="data4"><img id="4" onClick={flip} src={CardBack} /></td>
-                                <td id="data5"><img id="5" onClick={flip} src={CardBack} /></td>
-                                <td id="data6"><img id="6" onClick={flip} src={CardBack} /></td>
-                            </tr>
-                            <tr>
-                                <td id="data7"><img id="7" onClick={flip} src={CardBack} /></td>
-                                <td id="data8"><img id="8" onClick={flip} src={CardBack} /></td>
-                                <td id="data9"><img id="9" onClick={flip} src={CardBack} /></td>
-                                <td id="data10"><img id="10" onClick={flip} src={CardBack} /></td>
-                                <td id="data11"><img id="11" onClick={flip} src={CardBack} /></td>
-                                <td id="data12"><img id="12" onClick={flip} src={CardBack} /></td>
-                                <td id="data13"><img id="13" onClick={flip} src={CardBack} /></td>
-                            </tr>
-                            <tr>
-                                <td id="data14"><img id="14" onClick={flip} src={CardBack} /></td>
-                                <td id="data15"><img id="15" onClick={flip} src={CardBack} /></td>
-                                <td id="data16"><img id="16" onClick={flip} src={CardBack} /></td>
-                                <td id="data17"><img id="17" onClick={flip} src={CardBack} /></td>
-                                <td id="data18"><img id="18" onClick={flip} src={CardBack} /></td>
-                                <td id="data19"><img id="19" onClick={flip} src={CardBack} /></td>
-                                <td id="data20"><img id="20" onClick={flip} src={CardBack} /></td>
-                            </tr>
-                            <tr>
-                                <td id="data21"><img id="21" onClick={flip} src={CardBack} /></td>
-                                <td id="data22"><img id="22" onClick={flip} src={CardBack} /></td>
-                                <td id="data23"><img id="23" onClick={flip} src={CardBack} /></td>
-                                <td id="data24"><img id="24" onClick={flip} src={CardBack} /></td>
-                                <td id="data25"><img id="25" onClick={flip} src={CardBack} /></td>
-                                <td id="data26"><img id="26" onClick={flip} src={CardBack} /></td>
-                                <td id="data27"><img id="27" onClick={flip} src={CardBack} /></td>
-                            </tr>
-                        </table>
+                                    {/* <Button onClick={SingleMatchingGame}>Play Again</Button>
+                                    <Button onClick={GoHome}>Home</Button> */}
+                                </Alert>
+
+                                <h3 id="userPlayResult">~~~~~~~~~~Concentration~~~~~~~~~~</h3>
+
+                                <table cellSpacing="0">
+                                    <tr>
+                                        <td id="data0"><img id="0"  src={CardBack} /></td>
+                                        <td id="data1"><img id="1"  src={CardBack} /></td>
+                                        <td id="data2"><img id="2"  src={CardBack} /></td>
+                                        <td id="data3"><img id="3"  src={CardBack} /></td>
+                                        <td id="data4"><img id="4"  src={CardBack} /></td>
+                                        <td id="data5"><img id="5"  src={CardBack} /></td>
+                                        <td id="data6"><img id="6"  src={CardBack} /></td>
+                                    </tr>
+                                    <tr>
+                                        <td id="data7"><img id="7"  src={CardBack} /></td>
+                                        <td id="data8"><img id="8"  src={CardBack} /></td>
+                                        <td id="data9"><img id="9"  src={CardBack} /></td>
+                                        <td id="data10"><img id="10"  src={CardBack} /></td>
+                                        <td id="data11"><img id="11"  src={CardBack} /></td>
+                                        <td id="data12"><img id="12"  src={CardBack} /></td>
+                                        <td id="data13"><img id="13"  src={CardBack} /></td>
+                                    </tr>
+                                    <tr>
+                                        <td id="data14"><img id="14"  src={CardBack} /></td>
+                                        <td id="data15"><img id="15"  src={CardBack} /></td>
+                                        <td id="data16"><img id="16"  src={CardBack} /></td>
+                                        <td id="data17"><img id="17"  src={CardBack} /></td>
+                                        <td id="data18"><img id="18"  src={CardBack} /></td>
+                                        <td id="data19"><img id="19" src={CardBack} /></td>
+                                        <td id="data20"><img id="20"  src={CardBack} /></td>
+                                    </tr>
+                                    <tr>
+                                        <td id="data21"><img id="21"  src={CardBack} /></td>
+                                        <td id="data22"><img id="22"  src={CardBack} /></td>
+                                        <td id="data23"><img id="23"  src={CardBack} /></td>
+                                        <td id="data24"><img id="24"  src={CardBack} /></td>
+                                        <td id="data25"><img id="25"  src={CardBack} /></td>
+                                        <td id="data26"><img id="26"  src={CardBack} /></td>
+                                        <td id="data27"><img id="27"  src={CardBack} /></td>
+                                    </tr>
+                                </table>
+                            </>
+                        
+                        ) : (
+                            <>
+                            <div class="card-table-div">
+
+                                <h3 id="userPlayResult">~~~~~~~~~~Concentration~~~~~~~~~~</h3>
+
+                                <table cellSpacing="0">
+                                    <tr>
+                                        <td id="data0"><img id="0" onClick={flip} src={CardBack} /></td>
+                                        <td id="data1"><img id="1" onClick={flip} src={CardBack} /></td>
+                                        <td id="data2"><img id="2" onClick={flip} src={CardBack} /></td>
+                                        <td id="data3"><img id="3" onClick={flip} src={CardBack} /></td>
+                                        <td id="data4"><img id="4" onClick={flip} src={CardBack} /></td>
+                                        <td id="data5"><img id="5" onClick={flip} src={CardBack} /></td>
+                                        <td id="data6"><img id="6" onClick={flip} src={CardBack} /></td>
+                                    </tr>
+                                    <tr>
+                                        <td id="data7"><img id="7" onClick={flip} src={CardBack} /></td>
+                                        <td id="data8"><img id="8" onClick={flip} src={CardBack} /></td>
+                                        <td id="data9"><img id="9" onClick={flip} src={CardBack} /></td>
+                                        <td id="data10"><img id="10" onClick={flip} src={CardBack} /></td>
+                                        <td id="data11"><img id="11" onClick={flip} src={CardBack} /></td>
+                                        <td id="data12"><img id="12" onClick={flip} src={CardBack} /></td>
+                                        <td id="data13"><img id="13" onClick={flip} src={CardBack} /></td>
+                                    </tr>
+                                    <tr>
+                                        <td id="data14"><img id="14" onClick={flip} src={CardBack} /></td>
+                                        <td id="data15"><img id="15" onClick={flip} src={CardBack} /></td>
+                                        <td id="data16"><img id="16" onClick={flip} src={CardBack} /></td>
+                                        <td id="data17"><img id="17" onClick={flip} src={CardBack} /></td>
+                                        <td id="data18"><img id="18" onClick={flip} src={CardBack} /></td>
+                                        <td id="data19"><img id="19" onClick={flip} src={CardBack} /></td>
+                                        <td id="data20"><img id="20" onClick={flip} src={CardBack} /></td>
+                                    </tr>
+                                    <tr>
+                                        <td id="data21"><img id="21" onClick={flip} src={CardBack} /></td>
+                                        <td id="data22"><img id="22" onClick={flip} src={CardBack} /></td>
+                                        <td id="data23"><img id="23" onClick={flip} src={CardBack} /></td>
+                                        <td id="data24"><img id="24" onClick={flip} src={CardBack} /></td>
+                                        <td id="data25"><img id="25" onClick={flip} src={CardBack} /></td>
+                                        <td id="data26"><img id="26" onClick={flip} src={CardBack} /></td>
+                                        <td id="data27"><img id="27" onClick={flip} src={CardBack} /></td>
+                                    </tr>
+                                </table>
+                            </div>
+
+                            <br/>
+
+                            <div class="user-view-div" style={{textAlign: "left"}}>
+                                <p>
+                                    <strong>Number of Guesses: </strong> 
+                                    {userMoves}
+                                </p>
+                                <p>
+                                    <strong>Matches Made: </strong>
+                                    {matches}
+                                </p>
+                            </div>
+                            </>
+                    )}
                     </div>
-
-                    <br/>
-
-                    <div class="user-view-div" style={{textAlign: "left"}}>
-                        <p>
-                            <strong>Number of Guesses: </strong> 
-                            {userMoves}
-                        </p>
-                        <p>
-                            <strong>Matches Made: </strong>
-                            {matches}
-                        </p>
-                    </div>
-
-                </div>
+                
 
             </div>
 
@@ -252,27 +316,5 @@ export default function SingleMatchingGame({ token }){
     )
 }
 
-function GameOverAlert() {
-    const [show, setShow] = useState(true);
-  
-    return (
-      <>
-        <Alert show={show} variant="success">
-          <Alert.Heading>How's it going?!</Alert.Heading>
-          <p>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
-            lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
-            fermentum.
-          </p>
-          <hr />
-          <div className="d-flex justify-content-end">
-            <Button onClick={() => setShow(false)} variant="outline-success">
-              Close me y'all!
-            </Button>
-          </div>
-        </Alert>
-  
-        {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
-      </>
-    );
-  }
+
+
