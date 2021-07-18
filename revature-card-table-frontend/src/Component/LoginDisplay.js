3ec6f22ab623da94ae45bcb7b901e79d8d10c702
vincedgm <vince.012999@gmail.com>
2021-07-17 22:40:52 -0400

    Redirect to Login if not logged in

--- revature-card-table-frontend/src/Component/LoginDisplay.js
+++ revature-card-table-frontend/src/Component/LoginDisplay.js
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { Form } from "react-bootstrap";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 //import { render } from "@testing-library/react";
 import "../CSS/LoginDisplay.css";
 import { motion } from "framer-motion";
@@ -13,6 +13,7 @@ import { motion } from "framer-motion";
 export default function LoginDisplay({ setToken }) {
   const { register, handleSubmit } = useForm();
   const history = useHistory();
+  const location = useLocation();
 
   const onSubmit = (user) => {
     if (user.username && user.password) {
@@ -44,7 +45,14 @@ export default function LoginDisplay({ setToken }) {
               last_name: `${response.data.lastName}`,
               isLoggedIn: true,
             });
-            history.push("/");
+
+            if (location.state) {
+              history.push({
+                pathname: location.state.from.pathname
+              })
+            } else {
+              history.push("/");
+            }
           }
         })
         .catch((error) => {
