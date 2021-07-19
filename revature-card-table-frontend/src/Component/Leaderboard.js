547737de2867545b9f5a93c7843f71528a309b6c
molliemorrow <mfmorrow97@gmail.com>
2021-07-18 19:19:36 -0700

    Update Leaderboard functionality on front and back end

--- revature-card-table-frontend/src/Component/Leaderboard.js
+++ revature-card-table-frontend/src/Component/Leaderboard.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import "../CSS/Leaderboard.css";
 export default function Leaderboard() {
   const [gamestats, updateGamestats] = useState(null);
+  const [gamestatsAsc, updateGamestatsAsc] = useState(null);
+  const [gamestatsTime, updateGamestatsTime] = useState(null);
 
   useEffect(() => {
     fetch(
@@ -12,7 +14,25 @@ export default function Leaderboard() {
     console.log(gamestats);
   }, []);
 
-  return gamestats === null ? (
+  useEffect(() => {
+    fetch(
+      "http://localhost:8080/revature-card-table/leaderboard/find-all-by-points-asc"
+    )
+      .then((response) => response.json())
+      .then(updateGamestatsAsc);
+    console.log(gamestatsAsc);
+  }, []);
+
+  useEffect(() => {
+    fetch(
+      "http://localhost:8080/revature-card-table/leaderboard/find-all-by-time"
+    )
+      .then((response) => response.json())
+      .then(updateGamestatsTime);
+    console.log(gamestatsTime);
+  }, []);
+
+  return gamestats === null && gamestatsAsc === null && gamestatsTime === null ? (
     <div id="page-layout" class="container-fluid">
       <div id="leader-area">
         <h1>Loading Stats...</h1>
@@ -94,13 +114,13 @@ export default function Leaderboard() {
                 <td className="leader-td">User Username</td>
                 <td className="leader-td">User First Name</td>
                 <td className="leader-td">User Last Name</td>
-                <td className="leader-td">Points</td>
+                <td className="leader-td">Guesses</td>
                 <td className="leader-td">Date</td>
                 <td className="leader-td">Game Length</td>
               </tr>
             </thead>
             <tbody>
-              {gamestats.map((gamestat) => {
+              {gamestatsAsc.map((gamestat) => {
                 if (gamestat.won && gamestat.gameName === "matching-game") {
                   return (
                     <tr>
@@ -118,7 +138,43 @@ export default function Leaderboard() {
             </tbody>
           </table>
         </div>
+
+        <div>
+          <h2>Pickup Leaderboard</h2>
+            <table>
+              <thead>
+                <tr>
+                  <td className="leader-td">Game ID</td>
+                  <td className="leader-td">User Username</td>
+                  <td className="leader-td">User First Name</td>
+                  <td className="leader-td">User Last Name</td>
+                  <td className="leader-td">Points</td>
+                  <td className="leader-td">Date</td>
+                  <td className="leader-td">Game Length</td>
+                </tr>
+              </thead>
+              <tbody>
+                {gamestatsTime.map((gamestat) => {
+                  if (gamestat.won && gamestat.gameName === "pickup") {
+                    return (
+                      <tr>
+                        <td>{gamestat.id}</td>
+                        <td>{gamestat.user.username}</td>
+                        <td>{gamestat.user.firstName}</td>
+                        <td>{gamestat.user.lastName}</td>
+                        <td>{gamestat.points}</td>
+                        <td>{gamestat.datePlayed}</td>
+                        <td>{gamestat.timeCompleted}</td>
+                      </tr>
+                    );
+                  }
+                })}
+              </tbody>
+            </table>
+        </div>
+        
       </div>
+
     </>
   );
 }
